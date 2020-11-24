7e9f08c161e814227d4def5cadd8f989b13bbc54
neguezaymen <neguezaymen@gmail.com>
2020-11-24 15:38:56 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,5 @@
+import react from "react";
+const Address = () =>{
+    return <p>7 rue de l'école Menzel Kamel 5013</p>;
+};
+export default Address;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ProfilPhoto = () => {
+return <img src={"/me.jpg"} alt="Neguez Aymen"/> ;
+};
+export default ProfilPhoto;
